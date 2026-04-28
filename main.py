6fd8a73bdefee0435864440944911339c9990b4e
Matheus-Sakamae <jujitsu_fios_6h@icloud.com>
2026-04-28 15:32:23 -0300

    Criação do menu de opção
    Criação do menu de opção

---
+++ main.py
@@ -0,0 +1,12 @@
+def menu():
+    print(''''
+          1-Adicionar gasto
+          2-Ver gastos
+          3-Ver total de gastos 
+          0-Sair 
+          
+          
+          '''
+)
+    opc = int(input('Digite a opção desejada: '))
+menu()
