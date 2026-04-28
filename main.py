75c127b7b70e3485284016d427f5598aebb2aae1
Matheus-Sakamae <jujitsu_fios_6h@icloud.com>
2026-04-28 15:45:00 -0300

    Adicionado loop para caso o usuário digite uma opção inválida
    Adicionado loop para caso o usuário digite uma opção inválida

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 def menu():
-    print(''''
+    opc = 0
+    while opc != 1 and opc != 2 and opc != 3 and opc != 4:
+
+        print(''''
           1-Adicionar gasto
           2-Ver gastos
           3-Ver total de gastos 
-          0-Sair 
+          4-Sair 
           
           
           '''
-)
-    opc = int(input('Digite a opção desejada: '))
+                )
+        opc = int(input('Digite a opção desejada: '))
+        if opc == 1:
+            gasto = float(input(" Digite o valor do gasto: "))
+            print(f"Gasto de R${gasto} adicionado com sucesso!")
+        elif opc == 2:
+            print("Exibindo gastos...")
+        elif opc == 3:
+            print("Exibindo total de gastos...")
+        elif opc == 4:
+            print("Saindo do programa...")
+        if opc != 1 and opc != 2 and opc != 3 and opc != 4:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
 menu()
