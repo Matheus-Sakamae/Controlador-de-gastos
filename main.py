d0fee46ec2138662e25961ba43bb65335469a7fd
Matheus-Sakamae <jujitsu_fios_6h@icloud.com>
2026-04-28 15:48:19 -0300

    Criado cadastro completo de gastos
    Criado cadastro completo de gastos

--- main.py
+++ main.py
@@ -13,14 +13,21 @@ def menu():
                 )
         opc = int(input('Digite a opção desejada: '))
         if opc == 1:
-            gasto = float(input(" Digite o valor do gasto: "))
-            print(f"Gasto de R${gasto} adicionado com sucesso!")
+            nome_compra = input("Digite o nome da compra: ")
+            gasto = float(input("Digite o valor da compra: "))
+            data = input("Digite a data da compra (dd/mm/aaaa): ")
+            categoria = input("Digite a categoria da compra: ")
+            print("Gasto adicionado com sucesso!")
+
         elif opc == 2:
             print("Exibindo gastos...")
+
         elif opc == 3:
             print("Exibindo total de gastos...")
+
         elif opc == 4:
             print("Saindo do programa...")
+
         if opc != 1 and opc != 2 and opc != 3 and opc != 4:
             print("Opção inválida. Por favor, escolha uma opção válida.")
 
