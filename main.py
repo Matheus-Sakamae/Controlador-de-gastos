d836ef0042fa96e04c2a37a54607ae4c785f802a
Matheus-Sakamae <jujitsu_fios_6h@icloud.com>
2026-04-29 09:13:03 -0300

    Adicionada opcao de atualizar gastos
    Adicionada opcao de atualizar gastos

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ def menu():
           2-Ver gastos
           3-Ver total de gastos 
           4-Apagar gastos
-          5-Sair
+          5-Atualizar gastos
+          6-Sair
           
           
           '''
@@ -80,9 +81,32 @@ def menu():
                 menu()
             
         elif opc == 5:
+            query = "SELECT * FROM cadastro_gastos"
+            conn = get_connection()
+            cursor = conn.cursor()
+            cursor.execute(query)
+            gastos = cursor.fetchall()
+            for gasto in gastos:
+                print(f"ID: {gasto[0]}, Nome da Compra: {gasto[1]}, Valor: {gasto[2]}, Data: {gasto[3]}, Categoria: {gasto[4]}")
+            print("Digite o ID do gasto que deseja atualizar: ")
+            id_compra = int(input())
+            nome_compra = input("Digite o novo nome da compra: ")
+            preco_compra = float(input("Digite o novo valor da compra: "))
+            data_compra = datetime.strptime(input("Digite a nova data (dd/mm/aaaa): "), "%d/%m/%Y").date().isoformat()
+            categoria_compra = input("Digite a nova categoria da compra: ")
+            query = "UPDATE cadastro_gastos SET nome_compra = %s, preco_compra = %s, data_compra = %s, categoria_compra = %s WHERE id_compra = %s"
+            cursor.execute(query, (nome_compra, preco_compra, data_compra, categoria_compra, id_compra))
+            conn.commit()
+            print("Gasto atualizado com sucesso!")
+            print("Pressione ENTER para voltar ao menu...")
+            enter = input()
+            if enter == "":
+                menu()
+                
+        elif opc == 6:
             print("Saindo do programa...")
 
-        if opc != 1 and opc != 2 and opc != 3 and opc != 4 and opc != 5:
+        if opc != 1 and opc != 2 and opc != 3 and opc != 4 and opc != 5 and opc != 6:
             print("Opção inválida. Por favor, escolha uma opção válida.")
 
 menu()
